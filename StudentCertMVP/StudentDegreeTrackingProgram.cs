b55812c0618f44da06dcbab1539a64266da5c764
Zachary Grieser <zgrieser@uw.edu>
2022-05-07 21:52:04 -0700

    Adds new UI layout w/ some input

--- StudentCertMVP/StudentDegreeTrackingProgram.cs
+++ StudentCertMVP/StudentDegreeTrackingProgram.cs
@@ -12,6 +12,10 @@ namespace StudentCertMVP
 {
     public partial class StudentDegreeTrackingProgram : Form
     {
+        //initializes variables to store user input
+        string studentID = "";
+        string studentSched = "";
+
         public StudentDegreeTrackingProgram()
         {
             InitializeComponent();
@@ -20,6 +24,18 @@ namespace StudentCertMVP
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
             System.Diagnostics.Process.Start("c:\\");
+        }
+
+        //stores user input ID on btn click
+        private void idBtn_Click(object sender, EventArgs e)
+        {
+            studentID = idInputBox.Text;
+        }
+
+        //stores user input schedule on btn click
+        private void enterScheduleBtn_Click(object sender, EventArgs e)
+        {
+            studentSched = scheduleInputBox.Text;
         }
     }
 }
