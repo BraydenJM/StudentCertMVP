6e4ea93e6e638d2fb31a795e29196f16c6579be5
BraydenJM <35589837+BraydenJM@users.noreply.github.com>
2022-05-08 21:00:12 -0700

    added ability to apend notes. Changed properly closed filestreams to try to prevent file corruption

--- StudentCertMVP/Tracker.cs
+++ StudentCertMVP/Tracker.cs
@@ -70,10 +70,12 @@ namespace StudentCertMVP
             //col 3 = quarter taken
             //col 4 = notes
             XSSFWorkbook workbook = new XSSFWorkbook(fileStream);
+            fileStream.Close();
             List<string> matchedClasses = new List<string>();    
             ISheet x = workbook.GetSheet("Sheet1");
             for(int i = 22; i < x.LastRowNum; i++) //NOTE LOOP STARTS AT ROW 22 FOR TESTING ONLY
             {
+
                 if(x.GetRow(i) == null)
                 {
                     break;
@@ -85,14 +87,25 @@ namespace StudentCertMVP
                     {
                         if (x.GetRow(i).GetCell(3) == null || x.GetRow(i).GetCell(3).StringCellValue == "") //verify cell is emtpy
                         {
-                            x.GetRow(i).GetCell(3).SetCellValue(matchedClass.quarter);
+                            x.GetRow(i).GetCell(3).SetCellValue(matchedClass.quarter); //set course code
+                            x.GetRow(i).GetCell(2).SetCellValue(matchedClass.credit); //set credit amount
+                            matchedClasses.Add(matchedClass.classCode);
+                        }
+                        //course has been taken previously, handle note appending
+                        else
+                        {
+                            string cellValue = handleNotes(x.GetRow(i).GetCell(4).ToString());
+                            x.GetRow(i).GetCell(4).SetCellValue(cellValue);
+                            x.GetRow(i).GetCell(3).SetCellValue(matchedClass.quarter); //set course code
+                            x.GetRow(i).GetCell(2).SetCellValue(matchedClass.credit); //set credit amount
                             matchedClasses.Add(matchedClass.classCode);
                         }
                     }               
                 }
             }
-            fileStream = new FileStream(formPath, FileMode.Open, FileAccess.Write, FileShare.Write); //change stream mode to write
+            fileStream = new FileStream(formPath, FileMode.Open, FileAccess.Write, FileShare.Write);
             workbook.Write(fileStream);
+            fileStream.Close();
             string result = "Matched classes:\n";
             foreach(string matchedClass in matchedClasses)
             {
@@ -100,6 +113,118 @@ namespace StudentCertMVP
             }
 
             return result;
+        }
+        /// <summary>
+        /// Appends, inserts or increments the notes section string of the student tracker. If cell value is blank method return defaults to "Attempt 2"
+        /// if an attempt has already been made method will increment the attempt number. If other notes already exist, method appends attempt number to
+        /// the end of the note.
+        /// </summary>
+        /// <param name="noteValue">cell value of the tracker</param>
+        /// <returns>Incremented, appended, or newly created string to write to notes section</returns>
+        private string handleNotes(string noteValue)
+        {
+            //-----------------------------
+            //abandon hope all ye who enter
+            //-----------------------------
+            //verify something is actually contained in the notes other than a blank space or an accidental whitespace char
+            if(noteValue.Length > 1)
+            {
+                //check if an "attempt" note has already been added
+                if (noteValue.Contains("attempt", StringComparison.OrdinalIgnoreCase))
+                {
+                    //set attempt number to 0 as placeholder, split notes by whitespace
+                    int attempt = 0;
+                    string[] noteWords = noteValue.Split(" ");
+                    bool noteChanged = false;
+                    //iterate through each word in notes
+                    int i = 0;
+                    while (i < noteWords.Length && noteChanged == false)
+                    {
+                        //check if word is or contains "attempt"
+                        if (noteWords[i].Contains("attempt", StringComparison.Ordinal))
+                        { 
+                            //handle situations where there is no whitespace preceeding the attempt number
+                            for (int k = 0; k < noteWords[i].Length; k++)
+                            {
+                                if (Char.IsDigit(noteWords[i][k]))
+                                {
+                                    attempt = noteWords[i][k];
+                                    noteWords[i].Replace(",", "");
+                                    noteWords[i] = $"Attempt {attempt + 1}";
+                                    k = noteValue.Length + 1;
+                                    noteChanged = true;
+                                }
+                            }
+                            //white space preceeds the attempt number
+                            if (noteChanged == false)
+                            {
+                                //try catch blocks in case the attempt number was omitted entirely
+                                try
+                                {
+                                    //attempt to parse using existing functions
+                                    try
+                                    {
+                                        attempt = int.Parse(noteWords[i + 1]);
+                                        noteWords[i + 1] = $"{attempt + 1}";
+                                        noteChanged = true;
+                                    }
+                                    //invalid syntax. Typically caught when "nd" or "rd" is appended to the end of integers
+                                    catch
+                                    {
+                                        for (int k = 0; k < noteWords[i + 1].Length; k++)
+                                        {
+                                            if (Char.IsDigit(noteWords[i + 1][k]))
+                                            {
+                                                attempt = noteWords[i + 1][k];
+                                                noteWords[i + 1] = $"{attempt + 1}";
+                                                k = noteValue.Length + 1;
+                                                noteChanged = true;
+                                            }
+                                        }
+                                    }
+
+                                }
+                                catch
+                                {
+                                    //if all else fails, use Attempt 2 as default
+                                    return "Attempt 2";
+                                }
+                            }
+
+                        }
+                        i++;
+                    }
+                    //rebuild string and return
+                    if (noteChanged == true)
+                    {
+                        string result = "";
+                        for (int j = 0; j < noteWords.Length - 1; j++)
+                        {
+                            noteWords[j].Replace(",", "");
+                            noteWords[j].Replace(".", "");
+                            noteWords[j].Replace("_", "");
+                            result += noteWords[j] + ", ";
+                        }
+                        result += noteWords[noteWords.Length];
+                        return result;
+                    }
+                    //note was not changed for some reason change to default value
+                    else
+                    {
+                        return  "Attempt 2";
+                    }
+                }
+                //misc note already in cell. append default return value
+                else
+                {
+                    return noteValue += ", Attempt 2";
+                }
+            }
+            //empty cell value, return default
+            else
+            {
+                return  "Attempt 2";
+            }
         }
         /// <summary>
         /// iterates through parameter list of courses for matching course code provided in args
